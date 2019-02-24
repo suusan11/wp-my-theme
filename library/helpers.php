1378d89cd8764bb9785d84cd4f150bd65c17d788
suusan11 <gawow11@gmail.com>
2019-02-24 00:07:22 -0800

    add some escape tags

--- library/helpers.php
+++ library/helpers.php
@@ -8,14 +8,14 @@
 
 function my_theme_post_meta() {
     echo 'Posted on';
-    echo '<a href="'.get_permalink().'">';
-    echo '<time datetime="'.get_the_date('c').'">' .get_the_date().'</time>';
+    echo '<a href="'.esc_url(get_permalink()).'">';
+    echo '<time datetime="'.esc_attr(get_the_date('c')).'">' .esc_html(get_the_date()).'</time>';
     echo '</a>';
-    echo 'By <a href="'.get_author_posts_url( get_the_author_meta( 'ID' ) ).'">' .get_the_autor(). '</a>';
+    echo 'By <a href="'.esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )).'">' .esc_html(get_the_autor()). '</a>';
 }
 
 function my_theme_readmore_link() {
-    echo '<a href="'.get_the_permalink().'" title="'.the_title_attribute(['echo' => false]).'">';
+    echo '<a href="'.esc_url(get_the_permalink()).'" title="'.the_title_attribute(['echo' => false]).'">';
     echo 'Read More <span class="u-screen-reader-text">About '.the_title().'</span>';
     echo '</a>';
 }
