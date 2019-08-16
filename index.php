66d6fc64353dab19082a654b308a801e2bc9a0e7
suusan11 <gawow11@gmail.com>
2019-08-15 19:19:48 -0700

    add author parts

--- index.php
+++ index.php
@@ -10,32 +10,47 @@
 
 <?php get_header(); ?>
 
-    <?php if(have_posts()) { ?>
+<?php if (have_posts()) { ?>
 
-        <?php while(have_posts()) { ?>
+<?php while (have_posts()) { ?>
 
-            <?php the_post(); ?>
+<?php the_post(); ?>
 
-            <h2>
-                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-            </h2>
+<h2>
+    <a href="<?php the_permalink(); ?>"
+        title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+</h2>
 
-            <div>
-                <?php my_theme_post_meta(); ?>
-            </div>
-            <div>
-                <?php the_excerpt(); ?>
-            </div>
+<div>
+    Posted on
+    <a href="<?php echo get_permalink()?>">
+        <time datetime=<?php echo get_the_date('c')?>><?php echo get_the_date()?></time>
+    </a>
+    By <a
+        href="<?php echo get_author_posts_url(get_the_author_mata('ID'))?>">
+        <?php echo get_the_author(); ?>
+    </a>
+</div>
+<div>
+    <?php my_theme_post_meta(); ?>
+</div>
+<div>
+    <?php the_excerpt(); ?>
+</div>
+<a href="<?php echo get_the_permalink()?>"
+    title="<?php the_title_attribute(); ?>">
+    Read More <span class="u-screen-reader-text">About <?php the_title()?></span>
+</a>
+<?php my_theme_readmore_link(); ?>
+<?php } ?>
 
-            <?php my_theme_readmore_link(); ?>
-        <?php } ?>
+<?php the_posts_pagination(); ?>
+<?php } else { ?>
 
-        <?php the_posts_pagination(); ?>
-    <?php } else { ?>
+<p><?php __('no posts yet', 'mytheme'); ?>
+</p>
 
-        <p>no posts yet</p>
-
-    <?php } ?>
+<?php } ?>
 
 
-<?php get_footer(); ?>
+<?php get_footer();
