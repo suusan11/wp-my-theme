7cc666ad2a74571c549d8386135129db4fdc3b3a
suusan11 <gawow11@gmail.com>
2019-08-15 20:03:12 -0700

    update

--- functions.php
+++ functions.php
@@ -7,5 +7,3 @@
  */
 
 require_once('library/helpers.php');
-
-?>
--- index.php
+++ index.php
@@ -22,25 +22,11 @@
 </h2>
 
 <div>
-    Posted on
-    <a href="<?php echo get_permalink()?>">
-        <time datetime=<?php echo get_the_date('c')?>><?php echo get_the_date()?></time>
-    </a>
-    By <a
-        href="<?php echo get_author_posts_url(get_the_author_mata('ID'))?>">
-        <?php echo get_the_author(); ?>
-    </a>
-</div>
-<div>
     <?php my_theme_post_meta(); ?>
 </div>
 <div>
     <?php the_excerpt(); ?>
 </div>
-<a href="<?php echo get_the_permalink()?>"
-    title="<?php the_title_attribute(); ?>">
-    Read More <span class="u-screen-reader-text">About <?php the_title()?></span>
-</a>
 <?php my_theme_readmore_link(); ?>
 <?php } ?>
 
