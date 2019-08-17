05c2a5ec3c36d92e72e3f5c2a9bb53dd2b987c81
suusan11 <gawow11@gmail.com>
2019-08-17 16:06:39 -0700

    add function

--- library/enqueue-assets.php
+++ library/enqueue-assets.php
@@ -6,3 +6,10 @@ function my_theme_assets()
 }
 
 add_action('wp_enqueue_scripts', 'my_theme_assets');
+
+function my_theme_admin_assets()
+{
+    wp_enqueue_style('my_theme-admin-stylesheet', get_template_directory_uri() . '/dist.assets/css/admin.css', array(), '1.0.0', 'all');
+}
+
+add_action('admin_wp_enqueue_scripts', 'my_theme_assets');
