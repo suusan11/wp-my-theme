aeaf9466e98f1786d5a9353d89aef644aa13acdb
suusan11 <gawow11@gmail.com>
2019-08-17 15:38:19 -0700

    add some css packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,14 @@
 import gulp from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
+import cleanCSS from 'gulp-clean';
+import gulpif from 'gulp-if';
 
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = (done) => {
   return gulp.src('src/assets/scss/bundle.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulpif(PRODUCTION, cleanCSS({ compatibility: 'ie8' })))
     .pipe(gulp.dest('dist.assets/css'));
 }
