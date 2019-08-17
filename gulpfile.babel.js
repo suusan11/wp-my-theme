14b47f77948db21392ee633c8cf3dc43a8909abe
suusan11 <gawow11@gmail.com>
2019-08-17 16:01:11 -0700

    add new package

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,15 @@ import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean';
 import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
 
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = (done) => {
   return gulp.src('src/assets/scss/bundle.scss')
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulpif(PRODUCTION, cleanCSS({ compatibility: 'ie8' })))
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
     .pipe(gulp.dest('dist.assets/css'));
 }
