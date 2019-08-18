620ae63ff522d0004587535214bc19830f84ce18
suusan11 <gawow11@gmail.com>
2019-08-18 14:53:40 -0700

    add statement to js

--- library/enqueue-assets.php
+++ library/enqueue-assets.php
@@ -3,6 +3,7 @@
 function my_theme_assets()
 {
     wp_enqueue_style('my_theme-stylesheet', get_template_directory_uri() . '/dist.assets/css/bundle.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('my_theme-scripts', get_template_directory_uri() . '/dist/assets/js/bundle.js', array(), true);
 }
 
 add_action('wp_enqueue_scripts', 'my_theme_assets');
