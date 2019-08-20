1c1c6a0e0a4158ee19005f3e845bd7efd1066ad9
suusan11 <gawow11@gmail.com>
2019-08-20 14:42:52 -0700

    create child theme

--- functions.php
+++ functions.php
@@ -8,16 +8,3 @@
 
 require_once('library/helpers.php');
 require_once('library/enqueue-assets.php');
-
-function after_pagination()
-{
-    echo 'test';
-}
-add_action('_themename_after_pagination', 'after_pagination');
-
-
-function no_posta_text($text)
-{
-    return $text . 'filter test';
-}
-add_filter('_themename_no_post_text', 'no_posta_text');
--- library/helpers.php
+++ library/helpers.php
@@ -6,19 +6,21 @@
  * Time: 23:14
  */
 
-function _themename_post_meta()
-{
-    // tranlators %s: Post Data
-    printf(
-        esc_html__('Posted on %s', '_themename'),
-        '<a href="'.esc_url(get_permalink()).'"><time datetime="'.esc_attr(get_the_date('c')).'">' .esc_html(get_the_date()).'</time></a>'
-    );
-    // tranlators %s: Post Author
-    printf(
-        esc_html__(' By %s', '_themename'),
-        '<a href="'.esc_url(get_author_posts_url(get_the_author_meta('ID'))).'">' .esc_html(get_the_author()). '</a>'
-    );
-}
+ if (!function_exists('_themename_post_meta')) { //if statement for override by child theme
+     function _themename_post_meta()
+     {
+         // tranlators %s: Post Data
+         printf(
+             esc_html__('Posted on %s', '_themename'),
+             '<a href="'.esc_url(get_permalink()).'"><time datetime="'.esc_attr(get_the_date('c')).'">' .esc_html(get_the_date()).'</time></a>'
+        );
+         // tranlators %s: Post Author
+         printf(
+             esc_html__(' By %s', '_themename'),
+             '<a href="'.esc_url(get_author_posts_url(get_the_author_meta('ID'))).'">' .esc_html(get_the_author()). '</a>'
+        );
+     }
+ }
 
 function _themename_readmore_link()
 {
