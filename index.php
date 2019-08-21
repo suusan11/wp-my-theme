e93ab1c85c74846512868b099ced790f1fbfff8a
suusan11 <gawow11@gmail.com>
2019-08-20 17:08:34 -0700

    add some classes

--- index.php
+++ index.php
@@ -11,23 +11,23 @@
 <?php get_header(); ?>
 <div class="o-container u-margin-bottom-40">
     <div class="o-row">
-        <div class="o-row_column o-row_column--span-12 o-row_column--span-8@medium">
+        <div class="o-row__column o-row__column--span-12 o-row__column--span-8@medium">
             <main role='main'>
                 <?php if (have_posts()) { ?>
 
                 <?php while (have_posts()) { ?>
 
                 <?php the_post(); ?>
-                <article>
-                    <h2>
+                <article <?php post_class('c-post u-margin-bottom-20')?>>
+                    <h2 class="c-post__title">
                         <a href="<?php the_permalink(); ?>"
                             title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
                     </h2>
 
-                    <div>
+                    <div class="c-post__meta">
                         <?php _themename_post_meta(); ?>
                     </div>
-                    <div>
+                    <div class="c-post__excerpt">
                         <?php the_excerpt(); ?>
                     </div>
                     <?php _themename_readmore_link(); ?>
@@ -45,7 +45,7 @@
             </main>
         </div>
 
-        <div class="o-row_column o-row_column--span-12 o-row_column--span-4@medium">
+        <div class="o-row__column o-row__column--span-12 o-row__column--span-4@medium">
             <?php get_sidebar(); ?>
         </div>
     </div>
--- library/helpers.php
+++ library/helpers.php
@@ -24,7 +24,7 @@
 
 function _themename_readmore_link()
 {
-    echo '<a href="'.esc_url(get_the_permalink()).'" title="'.the_title_attribute(['echo' => false]).'">';
+    echo '<a class="c-post__readmore" href="'.esc_url(get_the_permalink()).'" title="'.the_title_attribute(['echo' => false]).'">';
     // tranlators %s: Post Title
     printf(
         wp_kses(
