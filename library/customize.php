5c5bc4e636502fc693b4569eabd75366444305d9
suusan11 <gawow11@gmail.com>
2019-08-28 15:23:49 -0700

    add customize function to footer

--- library/customize.php
+++ library/customize.php
@@ -1,6 +1,15 @@
 <?php
  function _themename_customize_register($wp_customize)
  {
+     $wp_customize -> add_section('_themename_footer_options', array(
+        'title' => esc_html__('Footer Options', '_themaname'),
+        'description' => esc_html__('You can change footer from here.', '_themaname')
+     ));
+
+
+
+
+
      $wp_customize -> add_setting('_themename_site_info', array(
       'default' => '',
       'sanitize_callback' => 'sanitize_text_field'
@@ -9,7 +18,7 @@
      $wp_customize -> add_control('_themename_site_info', array(
       'type' => 'text',
       'label' => esc_html__('Site Info', '_themaname'),
-      'section' => 'title_tagline'
+      'section' => '_themename_footer_options'
     ));
  }
 add_action('customize_register', '_themename_customize_register');
