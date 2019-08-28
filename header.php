47f706aeb5b5e932ce80aec70a0f2a6e11307122
suusan11 <gawow11@gmail.com>
2019-08-27 18:21:06 -0700

    add skip nav link

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-
+    <a class="u-skip-link" href="#content"><?php esc_attr_e('Skip to content', '_themename')?></a>
     <header rolw="banner" class="u-margin-bottom-40">
         <div class="c-header">
             <div class="o-container u-flex u-align-justify u-align-middle">
