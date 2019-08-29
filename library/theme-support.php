c17cda02f87ca4187cb19c0b3b9fd6121eda89f1
suusan11 <gawow11@gmail.com>
2019-08-28 20:37:27 -0700

    add function for refresh

--- library/theme-support.php
+++ library/theme-support.php
@@ -5,6 +5,7 @@ function _themename_theme_support()
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support('html5', array('search-form', 'comment-list', 'comment-form', 'gallery', 'caption'));
+    add_theme_support('customize-selective-refresh-widgrts');
 }
 
 add_action('after_setup_theme', '_themename_theme_support');
